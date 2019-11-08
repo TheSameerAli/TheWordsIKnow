e350834abb14448d15f4658e8878f3a3ee5fb5e6
Sameer Ali <sameer@silxo.com>
2019-11-08 23:33:24 +0000

    feat(timer): Adds timer mechanism in the game
    [Github]
    Issue #6

--- src/app/services/game/game-state.service.ts
+++ src/app/services/game/game-state.service.ts
@@ -1,4 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +10,17 @@ export class GameStateService {
   wordsAdded: string[] = [];
   correctWordCount;
   incorrectWordCount = 0;
+  elapsedTimeInSeconds = 0;
+  elapsedTime: Date = new Date(0, 0, 0, 0, 0, 0, 0);
 
   constructor() {
     this.correctWordCount = 0;
     this.incorrectWordCount = 0;
+    interval(1000).subscribe(() => {
+      this.elapsedTime = new Date(0, 0, 0, 0, 0, 0, 0);
+      this.elapsedTimeInSeconds++;
+      this.elapsedTime.setSeconds(this.elapsedTimeInSeconds);
+    });
   }
 
   addWord(word: string) {
