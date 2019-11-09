55d638abb1ce1b70ca7429ac66873ebea8fbb996
Sameer Ali <sameer@silxo.com>
2019-11-09 00:32:06 +0000

    feat(end-game): Adds end game button
    Changes made:
     - Adds end game button
     - Refactors game service into game-canvas component
     - Fixes time elapsed bug
    
    [Github]
    Issue #10

--- src/frontend/src/app/components/game-components/game-canvas/game-canvas.component.ts
+++ src/frontend/src/app/components/game-components/game-canvas/game-canvas.component.ts
@@ -1,6 +1,6 @@
 import { GameStateService } from './../../../services/game/game-state.service';
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game-canvas',
@@ -8,21 +8,82 @@ import { interval } from 'rxjs';
   styleUrls: ['./game-canvas.component.scss']
 })
 export class GameCanvasComponent implements OnInit {
+  wordsAdded: string[] = [];
+  correctWordCount = 0;
+  incorrectWordCount = 0;
+  started = false;
+  ended = false;
+
+  // Time mechanism
   public showStarting = false;
   public startingTimer = 3;
-  constructor(public gameStateService: GameStateService) { }
+  public timeElapsed: Date = new Date(0, 0, 0, 0, 0, 0, 0);
+  public timeElapsedInSeconds = 0;
+  public interval$: Subscription;
+  public gameStartingInterval$: Subscription;
+
+
+  constructor(public gameStateService: GameStateService) {
+    this.correctWordCount = 0;
+    this.incorrectWordCount = 0;
+    this.started = false;
+    this.timeElapsedInSeconds = 0;
+  }
 
   ngOnInit() {
   }
 
   startGame() {
+    this.ended = false;
+    this.wordsAdded = [];
+    this.correctWordCount = 0;
+    this.incorrectWordCount = 0;
     this.showStarting = true;
-    interval(1000).subscribe(time => {
+    this.gameStartingInterval$ = interval(1000).subscribe(time => {
       this.startingTimer -= 1;
       if (this.startingTimer <= 0) {
-        this.gameStateService.startGame();
+        this.started = true;
+        this.showStarting = false;
+        this.startTimer();
       }
     });
+  }
+
+  startTimer() {
+    this.interval$ = interval(1000).subscribe(() => {
+      this.timeElapsed = new Date(0, 0, 0, 0, 0, 0, 0);
+      this.timeElapsedInSeconds++;
+      this.timeElapsed.setSeconds(this.timeElapsedInSeconds);
+    });
+    this.gameStartingInterval$.unsubscribe();
+  }
+
+  endGame() {
+    this.started = false;
+    this.ended = true;
+    this.interval$.unsubscribe();
+    this.showStarting = false;
+    this.startingTimer = 3;
+  }
+
+  addWord(word: string) {
+    word = word.trim();
+    if (!word || word === '') {
+      return;
+    }
+    if (this.isValidWord(word)) {
+      this.wordsAdded.push(word);
+      this.correctWordCount++;
+    } else {
+      this.incorrectWordCount++;
+    }
+  }
+
+  private isValidWord(word: string) {
+    if (this.wordsAdded.includes(word)) {
+      return false;
+    }
+    return true;
   }
 
 }
